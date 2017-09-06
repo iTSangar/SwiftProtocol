bf06ae183f0d9783ca5b9d6f52f9fc7849adfe5e
Ítalo Sangar <italosangar@gmail.com>
2017-09-06 02:57:07 -0300

    update challenge content

--- Capacitacao_Protocol.playground/Pages/Desafio.xcplaygroundpage/Contents.swift
+++ Capacitacao_Protocol.playground/Pages/Desafio.xcplaygroundpage/Contents.swift
@@ -6,8 +6,15 @@
 
 import UIKit
 
-
-//: Transformar as animações em protocolos utilizando Programação Orientada a Protocolo para gerar os mesmos efeitos na tela atual. [Código fonte do desafio POP](https://github.com/iTSangar/SwiftProtocol)
+/*:
+ + Transformar as animações em protocolos utilizando Programação Orientada a Protocolo
+ 
+ + Todos os componentes da tela devem ter o efeito de buzz, além disso a mensagem de erro deve ter também o efeito de pop
+ 
+ + Animar os componentes no click do botão
+ 
+ */
+//: [Código fonte do desafio POP](https://github.com/iTSangar/SwiftProtocol/tree/master/challenge/protocolChallenge)
 
 
 /*:
