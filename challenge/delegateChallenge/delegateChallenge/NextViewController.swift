f8f024b387fdc7f833da927b4465d13893ecc0b8
Teobaldo Mauro de Moura <tmoura@ciandt.com>
2017-09-13 11:03:19 -0300

    Criação do projeto Desafio Delegate

---
+++ challenge/delegateChallenge/delegateChallenge/NextViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  closureChallenge
+//
+//  Created by Teobaldo Mauro de Moura on 23/08/17.
+//  Copyright © 2017 Teobaldo Mauro de Moura. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+    
+    // TODO: Create closure property
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func doSomethingTapped(_ sender: UIButton) {
+        print("Do something button tapped on Next view controller")
+        // TODO: Execute closure property
+    }
+
+}
+
