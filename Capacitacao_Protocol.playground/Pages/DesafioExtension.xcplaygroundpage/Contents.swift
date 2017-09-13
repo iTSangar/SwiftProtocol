22d2d559c742a1b88b0d0571a44a0e2c72ab3b19
Felipe Del Campo Gomes dos Santos <felipegs@ciandt.com>
2017-09-13 13:15:11 -0300

    Criada estrutura básica do desafio da extension com where

--- Capacitacao_Protocol.playground/Pages/DesafioExtension.xcplaygroundpage/Contents.swift
+++ Capacitacao_Protocol.playground/Pages/DesafioExtension.xcplaygroundpage/Contents.swift
@@ -2,6 +2,13 @@
 
 import Foundation
 
-var str = "Hello, playground"
+// TODO: Criar Protocolo que garante que garante a variável idade
 
+// TODO: Criar um Protocolo para comparar idades
+
+// TODO: Criar uma extension que implementa um método que recebe uma idade e compara ela com a idade salva no objeto atual
+
+// TODO: Criar um objeto que assina os dois protocolos criados anteriormente
+
+// TODO: Criar uma variável do tipo do objeto criado anteriormente e comparar a idade dela com outras idades
 //: [Next](@next)
