369a9ad888775434479a4395652d651630cbe4f8
Ítalo Sangar <italosangar@gmail.com>
2017-09-13 09:58:40 -0300

    advanced links

---
+++ Capacitacao_Protocol.playground/Pages/Advanced.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+
+//https://www.bobthedeveloper.io/blog/the-complete-understanding-of-swift-delegate-and-data-source
+
+// https://buildingvts.com/advanced-swift-protocols-d683cae12caf
+
+// https://github.com/heshamsalman/Animalia
+
+
+
+//: [Next](@next)
